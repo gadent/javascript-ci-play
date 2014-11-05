633189ebe79126f857e666f9b157b1dac2434ad0
Gavin Dent <gavindent82@gmail.com>
2014-11-05 12:11:46 +0000

    modified grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,6 +66,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-env');
     grunt.loadNpmTasks('grunt-shell-spawn');
+ 
 
     grunt.registerTask('build', 'bower');
     
